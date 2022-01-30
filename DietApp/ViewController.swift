9e90423d5741c4bf4ac93d26ae8f4250570d4f01
Futaku <futaku0811@gmail.com>
2022-01-30 19:28:45 +0900

    Initial Commit

---
+++ DietApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DietApp
+//
+//  Created by Takumi Fuzawa on 2022/01/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
